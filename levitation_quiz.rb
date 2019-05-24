0ce2dbed86447da131c5c0df69a773dfb02332a6
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-24 02:01:34 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,10 @@
 
 def levitation_quiz
-	puts = "What is the spell that enacs"
+	puts = "What is the spell that enacts levitation?"
+	spell = gets.chomp 
+	if spell == "Wingardium Leviosa"
+	  puts "You passed the quiz!"
+	  break
 end
 
 
